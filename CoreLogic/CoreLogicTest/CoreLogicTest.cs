a62d032b476b6690a37d5cb7ee9cb52bbc5963a7
Karen Royer <kmroyer@wpi.edu>
2019-03-28 10:47:47 -0400

    leap tour added
    use trigger button to travel from one 360 image to another

---
+++ CoreLogic/CoreLogicTest/CoreLogicTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreLogic;
+using CoreLogicTest;
+using Xunit;
+
+namespace CoreLogicTest
+{
+    public class CoreLogicTest
+    {
+        private ShellGameLogic CreateCoreLogic()
+        {
+            return new ShellGameLogic();
+        }
+        [Fact]
+        public void FirstTest()
+        {
+            //Arrange - s ystem u nder t est
+            ShellGameLogic sut = CreateCoreLogic();
+
+
+            //Act
+            bool result = sut.CheckForItem(2);
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
